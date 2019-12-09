cc1ee0201c30c8285761e4a6b092fecddec68887
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-09 16:23:24 +0700

    :recycle: Refactor face recognition folder

--- face-recognition/webcam.py
+++ face-recognition/webcam.py
@@ -49,11 +49,6 @@ while True:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.5)
             name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
