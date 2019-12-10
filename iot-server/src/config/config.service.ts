ec570fb4830b1cbcaa12500a0d763dd79d8c8959
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-10 14:07:22 +0700

    :sparkles: Add netpie environment variables

--- iot-server/src/config/config.service.ts
+++ iot-server/src/config/config.service.ts
@@ -19,4 +19,16 @@ export class ConfigService {
         return process.env.MYSQL_PASSWORD;
     }
 
+    public get netpieAppId(): string {
+        return process.env.NETPIE_APPID;
+    }
+
+    public get netpieKey(): string {
+        return process.env.NETPIE_KEY;
+    }
+
+    public get netpieSecret(): string {
+        return process.env.NETPIE_SECRET;
+    }
+
 }
