c32fb84b7643d3765c5a61f442b6c50126a93ba9
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-11 03:06:54 +0700

    :bug: Fix pi cam output frame

--- face-recognition/picam.py
+++ face-recognition/picam.py
@@ -2,7 +2,7 @@ import os
 import face_recognition
 import cv2
 import numpy as np
-import picamera
+from imutils.video import VideoStream
 
 face_dir = 'faces'
 
@@ -20,9 +20,8 @@ for face in known_faces_dir:
             known_face_encodings.append(face_recognition.face_encodings(person_image)[0])
             known_face_names.append(face)
 
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-output = np.empty((240, 320, 3), dtype=np.uint8)
+# video_capture = cv2.VideoCapture(0)
+video_capture  = VideoStream(src=0, usePiCamera=True).start()
 
 # Initialize some variables
 face_locations = []
@@ -31,11 +30,22 @@ face_names = []
 process_this_frame = True
 
 while True:
+    # Grab a single frame of video
+    frame = video_capture.read()
+    if frame is None:
+        continue
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(output)
-        face_encodings = face_recognition.face_encodings(output, face_locations)
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
@@ -57,10 +67,10 @@ while True:
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 5
-        right *= 5
-        bottom *= 5
-        left *= 5
+        # top *= 5
+        # right *= 5
+        # bottom *= 5
+        # left *= 5
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -71,7 +81,7 @@ while True:
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
-    cv2.imshow('Video', cv2.resize(frame, (0, 0), fx=0.5, fy=0.5))
+    cv2.imshow('Video', cv2.resize(frame, (0, 0), fx=1, fy=1))
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
